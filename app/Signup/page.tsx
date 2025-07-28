752a86293e20cf702131ce52412dddf71b27031b
marsyaaurl <2310512067@mahasiswa.upnvj.ac.id>
2025-07-28 15:22:34 +0700

    signup fixation

--- app/Signup/page.tsx
+++ app/Signup/page.tsx
@@ -1,13 +1,16 @@
 import SignUpForm from "../components/Signup/SignupForm";
 import NavbarLandingPage from "../components/LandingPage/NavbarLandingPage";
 import Footer from "../components/LandingPage/Footer";
+import { Suspense } from 'react';
 
 export default function SignupPage() {
     return (
         <>
             <div>
                 <NavbarLandingPage />
-                <SignUpForm />
+                <Suspense>
+                    <SignUpForm />
+                </Suspense>
                 <Footer />
             </div>
         </>
