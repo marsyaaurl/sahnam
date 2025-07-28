920f7a47c32fdcbf34f54aa41f492632eadd2ba2
marsyaaurl <2310512067@mahasiswa.upnvj.ac.id>
2025-07-28 15:14:16 +0700

    hmmm

--- app/Catalog/[id]/page.tsx
+++ app/Catalog/[id]/page.tsx
@@ -1,17 +1,19 @@
 import NavbarLoggedin from "@/app/components/NavbarLoggedin";
 import InvestForm from "@/app/components/Catalog/InvestForm";
 
-interface PageProps {
-  params: { id: string };
+interface RawParams {
+  id: string;
 }
 
-export default async function Invest({ params }: PageProps) {
+export default async function Invest({ params }: { params: Promise<RawParams> }) {
+  const realParams = await params;
+
   return (
     <div className="flex flex-row md:gap-x-48 px-5 md:px-0">
       <NavbarLoggedin />
       <div className="md:py-4 py-24">
         <h1 className="font-semibold text-4xl">Invest</h1>
-        <InvestForm plant_id={params.id} />
+        <InvestForm plant_id={realParams.id} />
       </div>
     </div>
   );
