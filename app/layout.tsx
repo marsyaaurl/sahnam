69de055644ecbef89efced2436f6ce7f7b8163d8
marsyaaurl <2310512067@mahasiswa.upnvj.ac.id>
2025-08-05 13:28:57 +0700

    maze script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AOSInit from './AOSInit';
 import SessionProvider from './SessionProvider';
+import Script from 'next/script'; // 👈 Tambahkan import ini
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,31 @@ export default function RootLayout({
         <SessionProvider>
           {children}
         </SessionProvider>
+
+        {/* Script Maze Tracking */}
+        <Script id="maze-tracking" strategy="afterInteractive">
+          {`
+            (function (m, a, z, e) {
+              var s, t;
+              try {
+                t = m.sessionStorage.getItem('maze-us');
+              } catch (err) {}
+
+              if (!t) {
+                t = new Date().getTime();
+                try {
+                  m.sessionStorage.setItem('maze-us', t);
+                } catch (err) {}
+              }
+
+              s = a.createElement('script');
+              s.src = z + '?apiKey=' + e;
+              s.async = true;
+              a.getElementsByTagName('head')[0].appendChild(s);
+              m.mazeUniversalSnippetApiKey = e;
+            })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '4f1d0561-5cac-446c-8367-f041b3cd400c');
+          `}
+        </Script>
       </body>
     </html>
   );
